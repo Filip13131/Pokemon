b0ed5a1189fe5f5e06196f3a5cfeb0465d391b0d
Filip Andrzej Kaminski <filip@Filips-MacBook-Pro.local>
2024-02-08 13:12:10 +0100

    added local storage solution

--- App.tsx
+++ App.tsx
@@ -5,27 +5,53 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FavoriteView } from './src/screens/FavoriteView/FavoriteView';
 import { MapView } from './src/screens/MapView/MapView';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FavoritePokemonsContext } from './src/context/FavoritePokemonsContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+
+  const getFavoritePokemons = async () : Promise<Array<string>> => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('favorites');
+      return jsonValue != null ? JSON.parse(jsonValue) : [];
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const storeFavoritePokemons = async (value) : Promise<void> => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem('favorites', jsonValue);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   const queryClient = new QueryClient();
   const TabNavigator = createBottomTabNavigator();
   const [favoritePokemons, setFavoritePokemons] = useState([]);
 
+  useEffect(()=>{
+    getFavoritePokemons().then((res)=>{setFavoritePokemons(res)})
+  },[])
 
   function updateFavoritePokemons(name: string){
+    let updatedFavoritePokemons;
     if(favoritePokemons.includes(name)){
-      const updatedFavoritePokemons = favoritePokemons.filter((val) => { 
+      updatedFavoritePokemons = favoritePokemons.filter((val) => { 
         return val!==name;
       });
       setFavoritePokemons(updatedFavoritePokemons);
       console.log(`favorite pokemon by name: ${name} removed`);
     }
     else{
-      setFavoritePokemons(favoritePokemons.concat([name]));
+      updatedFavoritePokemons = favoritePokemons.concat([name]);
+      setFavoritePokemons(updatedFavoritePokemons);
       console.log(`new favorite pokemon by name: ${name} added`);
     }
+    storeFavoritePokemons(updatedFavoritePokemons);
   }
 
   return (
